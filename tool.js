fe2bdb06a8bbbdb5173db2c81d8adb50b94fb954
Frank-K <frankdilank@hotmail.com>
2016-07-03 20:22:05 -0400

    Extension script

--- tool.js
+++ tool.js
@@ -1,8 +1,26 @@
 var pageLinks = document.links;
-var stuff = []
+var stuff = [];
+var bool = false;
+var link = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
 
 for (var num = 0; num < pageLinks.length; num++) {
-	stuff.push(pageLinks[num].href);
+	
+	if (pageLinks[num].href == link) {
+		bool = true;
+		
+		pageLinks[num].setAttribute('id', 'RickRoll');
+		pageLinks[num].setAttribute('class', 'hint--right hint--error');
+		pageLinks[num].setAttribute('aria-label', "You're about to get RickRoll'd!");
+		pageLinks[num].setAttribute('z-index', '999999999');
+	}
+
+
+
 }
 
-akkert(stuff);
+if (bool == true){
+	alert('YOU ARE ABOUT TO GET RICK ROLLED!');
+}
+else {
+	alert('REALX YOU ARE SAFE');
+}
